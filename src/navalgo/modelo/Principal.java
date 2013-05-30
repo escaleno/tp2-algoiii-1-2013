2cff8449544c5fb321259afbdc9510451118ca1e
msanti087 <msanti087@gmail.com>
2013-05-30 00:09:42 +0000

    probamos en clase el svn

--- src/navalgo/modelo/Principal.java
+++ src/navalgo/modelo/Principal.java
@@ -8,19 +8,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		
-		Lancha unaLancha;
-		Lancha otraLancha;
-		Punto unaPosicion;
-		Orientacion unaOrientacion;
-		Orientacion orientacionVertical;
-		unaPosicion = new Punto(3,2);
-		unaOrientacion = Orientacion.HORIZONTAL;
-		orientacionVertical = Orientacion.VERTICAL;
-		unaLancha = new Lancha(unaPosicion,unaOrientacion);
-		otraLancha = new Lancha(unaPosicion,orientacionVertical);
-		
-		DisparoConvencional unDisparo = new DisparoConvencional(unaPosicion);
-		unaLancha.asimilarDisparo(unDisparo);
+		System.out.println("hola");
 
 	}
 
