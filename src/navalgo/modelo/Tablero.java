9049a4c9de6c9d65903b12e761b13d8823dedd80
juanchoxl22@gmail.com <juanchoxl22@gmail.com@48d6c217-02cd-e874-8fb7-6611ad764b52>
2013-06-12 13:56:14 +0000

    muchachos, les paso el tablero con 9 test FUNCIONANDO, simplemente era mover de lugar el ejecutar Turno, en cuanto tenga mas hechos lo subo

---
+++ src/navalgo/modelo/Tablero.java
@@ -0,0 +1,127 @@
+package navalgo.modelo;
+import java.util.ArrayList;
+import java.util.Iterator;
+import navalgo.modelo.Barco;
+import navalgo.modelo.Disparo;
+
+public class Tablero 
+{
+	private ArrayList<Barco> listaBarcos;
+	private ArrayList<Disparo> disparosEfectuados;
+	private ArrayList<Barco> barcosHundidos;
+	
+	public Tablero()
+	{
+		listaBarcos=new ArrayList<Barco>();
+		disparosEfectuados=new ArrayList<Disparo>();
+		barcosHundidos=new ArrayList<Barco>();
+	}
+	
+	public void agregarBarco(Rompehielos b)
+	{
+		listaBarcos.add(b);
+	}
+	public void agregarBarco(Lancha b)
+	{
+		listaBarcos.add(b);
+	}
+	public void agregarBarco(Buque b)
+	{
+		listaBarcos.add(b);
+	}
+	
+	public void agregarBarco(Destructor b)
+	{
+		listaBarcos.add(b);
+	}
+
+	public void agregarBarco(Portaavion b)
+	{
+		listaBarcos.add(b);
+	}
+
+	
+	public void agregarDisparo(MinaSubmarinaPorContacto d)
+	{
+		disparosEfectuados.add(d);
+	}
+	
+	public void agregarDisparo(DisparoConvencional d)
+	{
+		disparosEfectuados.add(d);
+	}	
+	
+	public void agregarDisparo(MinaSubmarinaPuntualConRetardo d)
+	{
+		disparosEfectuados.add(d);
+	}
+	
+	public void agregarDisparo(MinaSubmarinaDobleConRetardo d)
+	{
+		disparosEfectuados.add(d);
+	}
+	
+	public void ejecutarTurno()
+	{
+		Iterator<Disparo> iteradorDisparos=disparosEfectuados.iterator();
+		while (iteradorDisparos.hasNext())
+		{	
+			Disparo disparoactual=iteradorDisparos.next();
+			Iterator<Barco> iteradorBarcos=listaBarcos.iterator();
+			while(iteradorBarcos.hasNext())
+			{	
+				Barco unBarco=iteradorBarcos.next();
+				disparoactual.atacar(unBarco);
+			}
+			
+		}
+		Iterator<Barco> iteradorBarcos=listaBarcos.iterator();
+		while (iteradorBarcos.hasNext())
+		{
+			Barco unBarco=iteradorBarcos.next();
+			if(unBarco.estaDestruido())
+			{
+				iteradorBarcos.remove();//remueve de la lista el elemento actual;
+				barcosHundidos.add(unBarco);
+			}
+			else
+			{
+				unBarco.mover();
+			}
+
+		}
+		
+		/*for (Disparo unDisparo: disparosEfectuados)
+		{
+			for (Barco unBarco: listaBarcos)
+			{
+				unDisparo.atacar(unBarco);
+			}
+		}
+		
+		for (Barco BarcoActual: listaBarcos)
+		{
+			if (BarcoActual.estaDestruido())
+			{
+				Barco barcoaAgregar=BarcoActual;
+				listaBarcos.remove(BarcoActual);
+				barcosHundidos.add(barcoaAgregar);
+			}
+		}*/
+	}	
+	
+	public ArrayList<Barco> getBarcos()
+	{
+		return listaBarcos;
+	}
+	
+	public ArrayList<Disparo> getDisparos()
+	{
+		return disparosEfectuados;
+	}
+	
+	public ArrayList<Barco> getDestruidos()
+	{
+		return barcosHundidos;
+	}
+}
