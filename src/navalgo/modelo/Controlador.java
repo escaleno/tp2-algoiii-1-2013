a2f243b968fed558e8756e94d506f3fa8dd348b6
jiesperon@gmail.com <jiesperon@gmail.com@48d6c217-02cd-e874-8fb7-6611ad764b52>
2013-06-13 09:01:06 +0000

    Saque todos los Warnings pero hay clases de mas que no se para que sirve.

--- src/navalgo/modelo/Controlador.java
+++ src/navalgo/modelo/Controlador.java
@@ -1,15 +1,15 @@
 package navalgo.modelo;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Controlador {
-	private ArrayList<Disparo> listaDeDisparos;
-	private ArrayList<Barco> listaDeBarcos;
-	
-	public Controlador(){
-		listaDeDisparos = new ArrayList<Disparo>();
-		listaDeBarcos = new ArrayList<Barco>();
-	}
+//	private ArrayList<Disparo> listaDeDisparos;
+//	private ArrayList<Barco> listaDeBarcos;
+//	
+//	public Controlador(){
+//		listaDeDisparos = new ArrayList<Disparo>();
+//		listaDeBarcos = new ArrayList<Barco>();
+//	}
 	
 	
 	
