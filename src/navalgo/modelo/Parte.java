a020f271eb9827741be041e1230d2896f15545f8
lezcano.jonatan <lezcano.jonatan@gmail.com>
2013-06-20 15:52:04 +0000

    A la clase Parte le agregue el atributo posicion para saber en que posicion esta en todo momento y no tener que estar calculandolo. Modifique ParteTest con pruebas nueva y cambie el nombre de algunas variables locales.

--- src/navalgo/modelo/Parte.java
+++ src/navalgo/modelo/Parte.java
@@ -1,15 +1,31 @@
 package navalgo.modelo;
 
 public class Parte {
+	
 	/*representa la cantidad de disparos que soporta, cuando
 	 * vale cero representa que fue totalmente destruida
 	 */
 	private int resistencia;
+	
+	/*
+	 * representa su posicion (x,y) en el tablero
+	 */
+	private Punto posicion;
+	
 		
-	/*constructor */
+	/*
+	 * ESTE CONSTRUCTOR DEBE SER BORRADO
+	 */
 	public Parte(int cantDeResistencia){
 		
 		this.resistencia = cantDeResistencia;
+	}
+	
+	/*constructor */
+	public Parte(int resistencia, Punto posicion){
+		
+		this.resistencia = resistencia;
+		this.posicion = posicion;
 	}
 	
 	public int getResistencia(){
@@ -22,6 +38,15 @@ public class Parte {
 		this.resistencia = nivelDeResistencia;
 		
 	}
+	
+	public Punto getPosicion(){
+		
+		return this.posicion;
+	}
+	
+	/*
+	 * retorna true si la parte tiene resistencia cero
+	 */
 	public boolean estaDestruida() {
 		return (this.resistencia == 0);
 	}
---
+++ src/navalgo/pruebas/ParteTest.java
@@ -0,0 +1,44 @@
+package navalgo.pruebas;
+
+import junit.framework.TestCase;
+import navalgo.modelo.*;
+
+public class ParteTest extends TestCase {
+	
+	private Parte parte;
+	protected void setUp() throws Exception {
+		super.setUp();
+		parte = new Parte(4, new Punto(3, 2));
+	}
+	
+	public void  testGetResistencia(){
+		assertEquals(4, parte.getResistencia());
+	}
+	
+	public void testGetPosicion(){
+		
+		assertEquals(new Punto(3, 2),parte.getPosicion());
+	}
+	
+	public void testSeConstruyeConUnaResistenciaYUnaPosicion(){
+		
+		assertEquals(4, parte.getResistencia());
+		assertEquals(new Punto(3, 2),parte.getPosicion());
+	}
+
+
+	public void testNoEstaDestruidoSiTieneResistenciaMayoraCero(){
+	assertFalse(parte.estaDestruida());
+	}
+	
+	public void testEstaDestruidaSiTieneResistenciaIgualACero(){
+		parte.setResistencia(0);
+		assertTrue(parte.estaDestruida());
+	}
+	
+	public void testSeBajaLaResistenciaEnUnoSiRecibeUnDisparoConvencional(){
+		parte.asimilarDisparo();
+		assertEquals(3,parte.getResistencia());
+	}
+	
+}
